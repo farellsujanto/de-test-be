331931e1284d53aef527a9de5634350e79da9091
farells <f.sujanto@gmail.com>
2022-09-23 01:41:21 +0700

    Statically move cats to owner for easier referencing

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -3,14 +3,7 @@ import { Cat } from '../graphql.schema';
 
 @Injectable()
 export class CatsService {
-  private readonly cats: Array<Cat & { ownerId?: number }> = [
-    { id: 1, name: 'Snowball', yearAge: 4, monthAge: 0, ownerId: 1, },
-    { id: 2, name: 'Blackly', yearAge: 2, monthAge: 3, ownerId: 1, },
-    { id: 3, name: 'Waffle', yearAge: 4, monthAge: 0, ownerId: 2, },
-    { id: 4, name: 'Blueberry', yearAge: 2, monthAge: 3, ownerId: 2, },
-    { id: 5, name: 'Strawberry', yearAge: 4, monthAge: 0, ownerId: 3, },
-    { id: 6, name: 'Mapple', yearAge: 2, monthAge: 3, ownerId: 4, },
-  ];
+  private readonly cats: Array<Cat & { ownerId?: number }> = [];
 
   create(cat: Cat): Cat {
     cat.id = this.cats.length + 1;
--- src/owners/owners.service.ts
+++ src/owners/owners.service.ts
@@ -5,10 +5,20 @@ import { Owner } from '../graphql.schema';
 @Injectable()
 export class OwnersService {
   private readonly owners: Owner[] = [
-    { id: 1, firstName: 'John', lastName: 'Lennon', isMaster: true, },
-    { id: 2, firstName: 'Paul', lastName: 'McCartney', isMaster: false, },
-    { id: 3, firstName: 'Georgia', lastName: 'Harrison', isMaster: false, },
-    { id: 4, firstName: 'Ringo', lastName: 'Starr', isMaster: false, },
+    { id: 1, firstName: 'John', lastName: 'Lennon', isMaster: true, cats: [
+        { id: 1, name: 'Snowball', yearAge: 4, monthAge: 0, },
+        { id: 2, name: 'Blackly', yearAge: 2, monthAge: 3, },
+    ] },
+    { id: 2, firstName: 'Paul', lastName: 'McCartney', isMaster: false, cats: [
+        { id: 3, name: 'Waffle', yearAge: 4, monthAge: 0, },
+        { id: 4, name: 'Blueberry', yearAge: 2, monthAge: 3, },
+    ] },
+    { id: 3, firstName: 'Georgia', lastName: 'Harrison', isMaster: false, cats: [
+        { id: 5, name: 'Strawberry', yearAge: 4, monthAge: 0, },
+    ] },
+    { id: 4, firstName: 'Ringo', lastName: 'Starr', isMaster: false, cats: [
+        { id: 6, name: 'Mapple', yearAge: 2, monthAge: 3, },
+    ] },
   ];
 
   setMaster(args: SetOwnnerAsMasterDto): Owner {
