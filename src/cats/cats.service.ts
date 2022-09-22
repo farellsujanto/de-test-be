b3002166ca6a9c5c50d14c44776aa945d3a82c1f
farells <f.sujanto@gmail.com>
2022-09-23 01:25:56 +0700

    Add new data to list of cats

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -4,7 +4,12 @@ import { Cat } from '../graphql.schema';
 @Injectable()
 export class CatsService {
   private readonly cats: Array<Cat & { ownerId?: number }> = [
-    { id: 1, name: 'Cat', age: 5, ownerId: 1 },
+    { id: 1, name: 'Snowball', yearAge: 4, monthAge: 0, ownerId: 1, },
+    { id: 2, name: 'Blackly', yearAge: 2, monthAge: 3, ownerId: 1, },
+    { id: 3, name: 'Waffle', yearAge: 4, monthAge: 0, ownerId: 2, },
+    { id: 4, name: 'Blueberry', yearAge: 2, monthAge: 3, ownerId: 2, },
+    { id: 5, name: 'Strawberry', yearAge: 4, monthAge: 0, ownerId: 3, },
+    { id: 6, name: 'Mapple', yearAge: 2, monthAge: 3, ownerId: 4, },
   ];
 
   create(cat: Cat): Cat {
